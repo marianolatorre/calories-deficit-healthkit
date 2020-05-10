4d592cc54b83d1986fad98a5d6e8febd7dd0a73c
mariano latorre <marianolatorre@gmail.com>
2020-05-10 21:19:36 +0100

    Initial Commit

---
+++ caloriesleftv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  caloriesleftv2
+//
+//  Created by mariano latorre on 10/05/2020.
+//  Copyright © 2020 mariano latorre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
